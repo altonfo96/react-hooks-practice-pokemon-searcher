37dcc3c4588c11e867a0a9b01bcedbdd306d7074
Alton Fo <altonfo109@gmail.com>
2022-12-23 11:48:13 -0500

    attempt

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,12 +2,15 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
-  return (
+function PokemonCollection({pokemons}) {
+  {pokemons.map((pokemon) => {
+    return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      <PokemonCard pokemon={pokemon}/>
     </Card.Group>
   );
+  })}
+  
 }
 
 export default PokemonCollection;
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
+
+
 function PokemonPage() {
+const [pokemons,setPokemons] = useState([])
+const [search, setSearch] = useState("")
+  useEffect(() => {
+    const request = async()=>{
+      let req = await fetch("http://localhost:3000/pokemon")
+      let res = await req.json()
+      console.log(res)
+      setPokemons(res)
+
+    }
+    request()
+  },[])
+  
+    const filterPokemon = (pokemons) => {
+      setPokemons(pokemons.filter((id) => {return id !== id}))
+        setPokemons([...filteredPokemon, pokemons])
+    }
+
+    
+    const handlePost = () => {
+      fetch("http://localhost:3000/pokemon", {method:"POST",
+      headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify})
+
+      .then((response) => response.json())
+      .then (data = response.json())
+      setPokemons(data)
+
+
+    }
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm handlePost = {handlePost}/>
       <br />
-      <Search />
+      <Search search = {search} setSearch = {setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemons} filterPokemon = {filterPokemon} />
     </Container>
   );
 }
